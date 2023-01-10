fd7057dbb8faa53be97a8eb7dd5c4b17bc665906
5991bnillok <kollinb95@gmail.com>
2023-01-10 14:36:31 -0600

    First Commit

---
+++ 4188C.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+#$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/TCL/4188C/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := 4188C
+PRODUCT_NAME := 4188C
+PRODUCT_BRAND := TCL
+PRODUCT_MODEL := 4188C
+PRODUCT_MANUFACTURER := TCL
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="TCL/4188C/Jetta_ATT:12/SP1A.210812.016/LV96:user/release-keys" \
+    PRIVATE_BUILD_DESC="sys_mssi_64_ww-user 12 SP1A.210812.016 LV96 release-keys"
